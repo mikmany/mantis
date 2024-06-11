f9dc6e1ce41a18fea17384cea0d8f90b917cc018
mikmany <mikhail.menyakin@gmail.com>
2024-06-12 01:46:01 +0300

    Взята "основа адреса" из конфигурационного файла target.json

--- fixture/soap.py
+++ fixture/soap.py
@@ -9,7 +9,8 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        url = self.app.config["web"]["baseUrl"]
+        client = Client(url + 'api/soap/mantisconnect.php?wsdl')
         try:
             client.service.mc_login(username, password)
             return True
@@ -17,7 +18,8 @@ class SoapHelper:
             return False
 
     def get_project_list(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        url = self.app.config["web"]["baseUrl"]
+        client = Client(url + 'api/soap/mantisconnect.php?wsdl')
 
         def convert(project):
             return Project(id=str(project.id), name=project.name, description=project.description)
